08460b8e2b8fc05ccf22ece1fc1bc15056a4992e
KC <weilihmen@gmail.com>
2018-01-24 18:00:45 +0800

    設計遊戲的戰鬥流程

--- js/index.js
+++ js/index.js
@@ -84,3 +84,52 @@ class Monster extends BaseCharacter {
 
 var hero = new Hero("Bernard", 130, 30);
 var monster = new Monster("Skeleton", 130, 10);
+
+//使skill ID點按時使出heroAttack
+function addSkillEvent() {
+  var skill = document.getElementById("skill");
+  skill.onclick = function() { 
+    heroAttack(); 
+  }
+}
+addSkillEvent();
+
+//回合設定
+var rounds = 10;
+function endTurn() {
+  rounds--;
+  document.getElementById("round-num").textContent = rounds;
+  if (rounds < 1) {
+    // 「遊戲結束」空白區
+  }
+}
+
+function heroAttack() {
+  document.getElementsByClassName("skill-block")[0].style.display = "none";
+
+  setTimeout(function() {
+    hero.element.classList.add("attacking");
+    setTimeout(function() {
+      hero.attack(monster);
+      hero.element.classList.remove("attacking");
+    }, 500);
+  }, 100);
+
+  setTimeout(function() {
+    if (monster.alive) {
+      monster.element.classList.add("attacking");
+      setTimeout(function() {
+        monster.attack(hero);
+        monster.element.classList.remove("attacking");
+        endTurn();
+        if (hero.alive == false) {
+          // 「遊戲結束」空白區
+        } else {
+          document.getElementsByClassName("skill-block")[0].style.display = "block";
+        }
+      }, 500);
+    } else {
+      // 「遊戲結束」空白區
+    }
+  }, 1100);
+}
